bcc197ec701252e7b457501b899ea8ced056d47d
dkirchhof <d.kirchhof@learning-by.de>
2019-08-30 15:32:59 +0200

    export result interfaces

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -3,24 +3,25 @@ export interface IAPI<T extends any> {
     callCached: (options: ICallCachedOptions) => CallFunction<T>;
     callMaybe: (options: ICallCachedOptions) => CallFunction<T>;
 }
-export declare type CallFunction<T extends any> = <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => Promise<ISuccess<ReturnType<T[K]>> | IFail | INoCall>;
+export declare type CallFunction<T extends any> = <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => Promise<CallResult<ReturnType<T[K]>>>;
+export declare type CallResult<T> = ISuccess<T> | IFail | INoCall;
 export interface ICallBaseOptions {
     onProgress?: (progress: number) => any;
 }
 export interface ICallCachedOptions extends ICallBaseOptions {
     cacheTime: number;
 }
-interface ISuccess<T = any> {
+export interface ISuccess<T = any> {
     type: "success";
     code: number;
     data: T;
 }
-interface IFail {
+export interface IFail {
     type: "fail";
     code: number;
     error: string;
 }
-interface INoCall {
+export interface INoCall {
     type: "noCall";
 }
 export declare class API<T extends any> implements IAPI<T> {
@@ -32,4 +33,3 @@ export declare class API<T extends any> implements IAPI<T> {
     callCached<K extends keyof T & string>(options: ICallCachedOptions): (procedure: K, ...params: Parameters<T[K]>) => Promise<IFail | ISuccess<any>>;
     callMaybe<K extends keyof T & string>(options: ICallCachedOptions): (procedure: K, ...params: Parameters<T[K]>) => Promise<IFail | INoCall | ISuccess<any>>;
 }
-export {};
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -6,7 +6,8 @@ export interface IAPI<T extends any> {
     callMaybe: (options: ICallCachedOptions) => CallFunction<T>;
 }
 
-export type CallFunction<T extends any> = <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => Promise<ISuccess<ReturnType<T[K]>> | IFail | INoCall>;
+export type CallFunction<T extends any> = <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => Promise<CallResult<ReturnType<T[K]>>>;
+export type CallResult<T> = ISuccess<T> | IFail | INoCall 
 
 export interface ICallBaseOptions {
     onProgress?: (progress: number) => any;
@@ -16,19 +17,19 @@ export interface ICallCachedOptions extends ICallBaseOptions {
     cacheTime: number;
 }
 
-interface ISuccess<T = any> {
+export interface ISuccess<T = any> {
     type: "success";
     code: number;
     data: T;
 }
 
-interface IFail {
+export interface IFail {
     type: "fail";
     code: number;
     error: string;
 }
 
-interface INoCall {
+export interface INoCall {
     type: "noCall";
 }
 
