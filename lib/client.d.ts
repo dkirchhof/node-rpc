d8fab4b8c6f5147c5eececb1ef1c106803be5f37
dkirchhof <d.kirchhof@learning-by.de>
2019-10-02 09:42:56 +0200

    createrequest function (better understanding, how the remote procedure is called)

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -2,6 +2,11 @@ export interface IAPI<T extends any> {
     call: (options?: ICallBaseOptions) => CallFunction<T>;
     callCached: (options: ICallCachedOptions) => CallFunction<T>;
     callMaybe: (options: ICallCachedOptions) => CallFunction<T>;
+    createRequest: <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => {
+        call: (options?: ICallBaseOptions) => Promise<ISuccess<ReturnType<T[K]>> | IFail>;
+        callCached: (options: ICallCachedOptions) => Promise<ISuccess<ReturnType<T[K]>> | IFail>;
+        callMaybe: (options: ICallCachedOptions) => Promise<ISuccess<ReturnType<T[K]>> | IFail | INoCall>;
+    };
 }
 export declare type CallFunction<T extends any> = <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => Promise<CallResult<ReturnType<T[K]>>>;
 export declare type CallResult<T> = ISuccess<T> | IFail | INoCall;
@@ -29,7 +34,21 @@ export declare class API<T extends any> implements IAPI<T> {
     private readonly cachedMaybes;
     private readonly cachedResponses;
     constructor(endpoint: string);
+    /**
+     * @deprecated
+     */
     call<K extends keyof T & string>(options?: ICallBaseOptions): (procedure: K, ...params: Parameters<T[K]>) => Promise<IFail | ISuccess<any>>;
+    /**
+     * @deprecated
+     */
     callCached<K extends keyof T & string>(options: ICallCachedOptions): (procedure: K, ...params: Parameters<T[K]>) => Promise<IFail | ISuccess<any>>;
+    /**
+     * @deprecated
+     */
     callMaybe<K extends keyof T & string>(options: ICallCachedOptions): (procedure: K, ...params: Parameters<T[K]>) => Promise<IFail | INoCall | ISuccess<any>>;
+    createRequest<K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>): {
+        call: (options?: ICallBaseOptions | undefined) => Promise<IFail | ISuccess<ReturnType<T[K]>>>;
+        callCached: (options: ICallCachedOptions) => Promise<IFail | ISuccess<ReturnType<T[K]>>>;
+        callMaybe: (options: ICallCachedOptions) => Promise<IFail | INoCall | ISuccess<ReturnType<T[K]>>>;
+    };
 }
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,10 +4,16 @@ export interface IAPI<T extends any> {
     call: (options?: ICallBaseOptions) => CallFunction<T>;
     callCached: (options: ICallCachedOptions) => CallFunction<T>;
     callMaybe: (options: ICallCachedOptions) => CallFunction<T>;
+
+    createRequest: <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => {
+        call: (options?: ICallBaseOptions) => Promise<ISuccess<ReturnType<T[K]>> | IFail>;
+        callCached: (options: ICallCachedOptions) => Promise<ISuccess<ReturnType<T[K]>> | IFail>;
+        callMaybe: (options: ICallCachedOptions) => Promise<ISuccess<ReturnType<T[K]>> | IFail | INoCall>;
+    };
 }
 
 export type CallFunction<T extends any> = <K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) => Promise<CallResult<ReturnType<T[K]>>>;
-export type CallResult<T> = ISuccess<T> | IFail | INoCall 
+export type CallResult<T> = ISuccess<T> | IFail | INoCall
 
 export interface ICallBaseOptions {
     onProgress?: (progress: number) => any;
@@ -37,10 +43,11 @@ export class API<T extends any> implements IAPI<T> {
     private readonly cachedMaybes = new Map<number, { date: number; }>();
     private readonly cachedResponses = new Map<number, { date: number; response: ISuccess<any>; }>();
 
-    constructor(private readonly endpoint: string) {
+    constructor(private readonly endpoint: string) { }
 
-    }
-
+    /**
+     * @deprecated
+     */
     public call<K extends keyof T & string>(options?: ICallBaseOptions) {
         return async (procedure: K, ...params: Parameters<T[K]>) => {
             const data = toFormData(params);
@@ -52,19 +59,19 @@ export class API<T extends any> implements IAPI<T> {
                 data,
             }
 
-            if(options && options.onProgress) {
+            if (options && options.onProgress) {
                 requestConfig.onUploadProgress = event => options.onProgress!(event.loaded / event.total);
             }
 
             const response = await request<ReturnType<T[K]>>(requestConfig);
 
-            if(response.code < 400) {
-                const result: ISuccess = { 
+            if (response.code < 400) {
+                const result: ISuccess = {
                     type: "success",
                     code: response.code,
                     data: response.data!,
                 };
-        
+
                 return result;
             } else {
                 const result: IFail = {
@@ -72,12 +79,15 @@ export class API<T extends any> implements IAPI<T> {
                     code: response.code,
                     error: response.error!,
                 };
-                
+
                 return result;
             }
         };
     }
 
+    /**
+     * @deprecated
+     */
     public callCached<K extends keyof T & string>(options: ICallCachedOptions) {
         return async (procedure: K, ...params: Parameters<T[K]>) => {
             const hash = createHash(JSON.stringify({ procedure, params }));
@@ -85,10 +95,10 @@ export class API<T extends any> implements IAPI<T> {
             const fromCache = this.cachedResponses.get(hash);
             const now = new Date().getTime();
 
-            if(!fromCache || now - fromCache.date > options.cacheTime) {
+            if (!fromCache || now - fromCache.date > options.cacheTime) {
                 const response = await this.call(options)(procedure, ...params);
-                
-                if(response.type === "success") {
+
+                if (response.type === "success") {
                     this.cachedResponses.set(hash, { date: now, response });
                 }
 
@@ -99,6 +109,9 @@ export class API<T extends any> implements IAPI<T> {
         };
     }
 
+    /**
+     * @deprecated
+     */
     public callMaybe<K extends keyof T & string>(options: ICallCachedOptions) {
         return async (procedure: K, ...params: Parameters<T[K]>) => {
             const hash = createHash(JSON.stringify({ procedure, params }));
@@ -106,10 +119,10 @@ export class API<T extends any> implements IAPI<T> {
             const fromCache = this.cachedMaybes.get(hash);
             const now = new Date().getTime();
 
-            if(!fromCache || now - fromCache.date > options.cacheTime) {
+            if (!fromCache || now - fromCache.date > options.cacheTime) {
                 const response = await this.call(options)(procedure, ...params);
-                
-                if(response.type === "success") {
+
+                if (response.type === "success") {
                     this.cachedMaybes.set(hash, { date: now });
                 }
 
@@ -123,18 +136,102 @@ export class API<T extends any> implements IAPI<T> {
             }
         };
     }
+
+    public createRequest<K extends keyof T & string>(procedure: K, ...params: Parameters<T[K]>) {
+        const call = async (options?: ICallBaseOptions): Promise<ISuccess<ReturnType<T[K]>> | IFail> => {
+            const data = toFormData(params);
+
+            const requestConfig: AxiosRequestConfig = {
+                url: this.endpoint,
+                method: "post",
+                headers: { "X-RPC-Procedure": procedure },
+                data,
+            }
+
+            if (options && options.onProgress) {
+                requestConfig.onUploadProgress = event => options.onProgress!(event.loaded / event.total);
+            }
+
+            const response = await request<any>(requestConfig);
+
+            if (response.code < 400) {
+                const result: ISuccess = {
+                    type: "success",
+                    code: response.code,
+                    data: response.data!,
+                };
+
+                return result;
+            } else {
+                const result: IFail = {
+                    type: "fail",
+                    code: response.code,
+                    error: response.error!,
+                };
+
+                return result;
+            }
+        };
+
+        const callCached = async (options: ICallCachedOptions): Promise<ISuccess<ReturnType<T[K]>> | IFail> => {
+            const hash = createHash(JSON.stringify({ procedure, params }));
+
+            const fromCache = this.cachedResponses.get(hash);
+            const now = new Date().getTime();
+
+            if (!fromCache || now - fromCache.date > options.cacheTime) {
+                const response = await call(options);
+
+                if (response.type === "success") {
+                    this.cachedResponses.set(hash, { date: now, response });
+                }
+
+                return response;
+            } else {
+                return fromCache.response;
+            }
+        };
+
+        const callMaybe = async (options: ICallCachedOptions): Promise<ISuccess<ReturnType<T[K]>> | IFail | INoCall> => {
+            const hash = createHash(JSON.stringify({ procedure, params }));
+
+            const fromCache = this.cachedMaybes.get(hash);
+            const now = new Date().getTime();
+
+            if (!fromCache || now - fromCache.date > options.cacheTime) {
+                const response = await call(options);
+
+                if (response.type === "success") {
+                    this.cachedMaybes.set(hash, { date: now });
+                }
+
+                return response;
+            } else {
+                const response: INoCall = {
+                    type: "noCall",
+                };
+
+                return response;
+            }
+        };
+
+        return {
+            call, callCached, callMaybe,
+        };
+    }
 }
+
 
 function toFormData(params: any[]) {
     const data = new FormData();
 
     const undefinedReplacer = (_: string, v: any) => v === undefined ? null : v;
-    
+
     params.forEach((p, i) => {
         const key = i.toString();
 
         if (p instanceof FileList) {
-            if(p.length === 0) {
+            if (p.length === 0) {
                 data.append(key, "null");
             } else {
                 [...p].forEach(file => data.append(key, file));
@@ -160,8 +257,8 @@ async function request<T>(config: AxiosRequestConfig): Promise<{ code: number; d
             data: result.data,
         }
     }
-    catch(e) {
-        if(e.response) {
+    catch (e) {
+        if (e.response) {
             return {
                 code: e.response.status,
                 error: e.response.data,
@@ -178,13 +275,19 @@ async function request<T>(config: AxiosRequestConfig): Promise<{ code: number; d
 function createHash(str: string) {
     let h = 5381;
     let i = str.length;
-  
-    while(i) {
+
+    while (i) {
         h = (h * 33) ^ str.charCodeAt(--i);
     }
-  
+
     /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
      * integers. Since we want the results to be always positive, convert the
      * signed int to an unsigned by doing an unsigned bitshift. */
     return h >>> 0;
 }
+
+const api = new API<{ test: (a: number, b: number) => number; }>("http://localhost:3000");
+
+
+(api as IAPI<{  test: (a: number, b: number) => number; }>).createRequest("test", 2, 4).call().then(r => { if (r.type === "success") r.data })
+// api.call()("test", 1, 2).then(r => { if(r.type === "success") r.data})
